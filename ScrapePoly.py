2ae453a6ca1d510ba047cb77c2646737b562aafd
Jim Groot <32303294+Jimgroot@users.noreply.github.com>
2019-12-02 00:54:14 +0200

    Add files via upload
    Scraping polygons from Copernicus

---
+++ ScrapePoly.py
@@ -0,0 +1,19 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+#Get the newest open activations
+url='https://emergency.copernicus.eu/mapping/activations-rapid/feed'
+resp = requests.get(url)
+soup=BeautifulSoup(resp.content, features='xml')
+
+#search for activations
+items =soup.findAll('item')
+first = items[0].title.text
+
+#Select only string values with activation Code
+activation = first[1:8]
+url='https://emergency.copernicus.eu/mapping/list-of-components/{}/aemfeed'.format(activation)
+resp = requests.get(url)
+soup=BeautifulSoup(resp.content, features='xml')
+#Scrape Polygons
+polygons =soup.findAll('georss:polygon')
